5e7799a4e5ce5fa626790123d37b0c8f6dce2422
USUARIO <USUARIO@LAPTOP-GTR2DQID>
2022-07-07 20:31:10 -0500

    prueba de poo 1.0

---
+++ src/ec/edu/intsuperior/modelo/Tanque.java
@@ -0,0 +1,34 @@
+package ec.edu.intsuperior.modelo;
+
+public class Tanque {
+    public void cantidadTripulacion (){
+        System.out.println("5");
+    }
+    public void Peso (){
+        System.out.println("87t");
+    }
+    public void blindaje (){
+        System.out.println("120mm");
+    }
+    public void cañon (){
+        System.out.println("105mm");
+    }
+    public void municion (){
+        System.out.println("75mm");
+    }
+    public static void comandante (){
+        System.out.println("Dirige a la tripulacion");
+    }
+    public static void conductor (){
+        System.out.println("Mnaeja el tanque");
+    }
+    public static void recargador (){
+        System.out.println("Recarga el cañon");
+    }
+    public static void artillero (){
+        System.out.println("disapta por el cañon");
+    }
+    public static void encargadoRadio (){
+        System.out.println("Se encarga de las comunicaciones");
+    }
+}
---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,10 @@
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Tanque;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        Tanque obj = new Tanque();
+        obj.cantidadTripulacion();
+    }
+}
